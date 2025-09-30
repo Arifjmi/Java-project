c6ab1ad30ac1ff91480c568bbcc4d1deaa0b9645
arif <reza.jmi.ece@gmail.com>
2025-09-30 12:26:31 +0000

    third commit

--- main.java
+++ main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Java World!");
-        System.out.println("This is my first simple project.");
     }
 }
 
